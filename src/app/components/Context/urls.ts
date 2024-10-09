679223066fb1298f277a994ad0ea9ff84d27f08c
krishnakashiv <krishna.kashiv@gmail.com>
2024-10-09 18:26:56 -0400

    Frontend with GPT enabled

---
+++ src/app/components/Context/urls.ts
@@ -0,0 +1,18 @@
+export const urls = [{
+  url: "https://e360.yale.edu/digest/indonesia-malaysia-deforestation",
+  title: "Indonesia Deforestation",
+  seeded: false,
+  loading: false,
+},
+{
+  url: "https://docs.python.org/3/tutorial/datastructures.html",
+  title: "Python Lists",
+  seeded: false,
+  loading: false,
+},
+{
+  url: "https://www.cbssports.com/nba/news/matisse-thybulle-to-stay-with-blazers-who-match-mavericks-three-year-33-million-deal-per-report/",
+  title: "Matisee Thybulle",
+  seeded: false,
+  loading: false,
+}]
